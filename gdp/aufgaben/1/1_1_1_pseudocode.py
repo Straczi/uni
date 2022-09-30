82a174a9cd3558a25883246d3f5d160df85a0ff8
flamion <laberschleichi@gmail.com>
2022-09-30 09:33:01 +0200

    add aufgabe 1

---
+++ gdp/aufgaben/1/1_1_1_pseudocode.py
@@ -0,0 +1,19 @@
+def wurzel(a):
+    x = a / 2
+
+    while True:
+        z = (a / x + x) / 2
+        d = (z - x) * (z - x)
+        
+        if d < 0.000_000_1:
+            break
+        
+        x = z
+
+    return x
+
+
+a = input("Number a: ")
+a = int(a)
+print(wurzel(a))
+
