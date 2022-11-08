569a552099a16909ffdb0a9308780764e55bd71b
flamion <flamion@protonmail.com>
2022-11-08 10:07:27 +0100

    Add Debug test file from examination

--- gdp/src/s5unit/RecursiveCircles.java
+++ gdp/src/s5unit/RecursiveCircles.java
@@ -5,13 +5,10 @@ import java.awt.Color;
 import std.ui.MiniApp;
 
 public class RecursiveCircles extends MiniApp {
-	
 	static int circleNumber = 0;
 	
-	
-	
 	public static void main(String[] args) {
-		drawCircles(100, 100, 50, 50);
+		drawCircles(500, 500, 500, 1);
 	}
 	
 	static void drawCircles(double x, double y, double radius, double minimumRadius) {
@@ -21,6 +18,7 @@ public class RecursiveCircles extends MiniApp {
 		
 		drawCircle("Kreis nr. " + circleNumber, (int) x, (int) y, (int) radius, Color.black);
 		circleNumber++;
+		
 		double newRadius = radius / (Math.sqrt(2) + 1);
 		drawCircles(x - newRadius, y + newRadius, newRadius, minimumRadius);
 		drawCircles(x + newRadius, y + newRadius, newRadius, minimumRadius);
