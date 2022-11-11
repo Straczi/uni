ec7d555dcf398f73a5d252972c2ed08bbf2f3729
flamion <flamion@protonmail.com>
2022-11-11 10:33:37 +0100

    Add GDP MergeSortedArrays task

---
+++ gdp/src/s6array/MergeSortedArrays.java
@@ -0,0 +1,35 @@
+package s6array;
+
+public class MergeSortedArrays {
+	public static void main(String[] args) {
+
+	}
+
+	public static void test(Function)
+	
+	public static int[] merge(int[] a, int[] b) {
+		int[] out = new int[a.length + b.length];
+		int aIndex = 0;
+		int bIndex = 0;
+		
+		for (int i = 0; i < out.length; i++) {
+			
+			if (aIndex < a.length && bIndex < b.length && a[aIndex] < b[bIndex]) {
+				out[i] = a[aIndex];
+				aIndex++;
+			} else if (aIndex < a.length && bIndex < b.length && b[bIndex] < a[aIndex]) {
+				out[i] = b[bIndex];
+				bIndex++;
+			} else if (aIndex < a.length) {
+				out[i] = a[aIndex];
+				aIndex++;
+			} else {
+				out[i] = b[bIndex];
+				bIndex++;
+			}
+		}
+		
+		
+		return out;
+	}
+}
