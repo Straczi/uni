3d4fec59734bf0c8f343beb5d80da8f714285bd2
flamion <flamion@protonmail.com>
2022-11-15 08:33:05 +0100

    Add GDP MostFrequent task

---
+++ gdp/src/s6array/MostFrequent.java
@@ -0,0 +1,40 @@
+package s6array;
+
+import java.util.Scanner;
+
+public class MostFrequent {
+	public static void main(String[] args) {
+		var scanner = new Scanner(System.in);
+		
+		int amount = scanner.nextInt();
+		int[] numbers = new int[amount];
+		
+		for (int i = 0; i < amount; i++) {
+			numbers[i] = scanner.nextInt();
+		}
+		
+		int mostFrequent = mostfreq(numbers);
+		System.out.println("Most frequent = " + mostFrequent);
+	}
+	
+	public static int mostfreq(int[] a) {
+		// Wäre jetzt eine HashMap angebracht?
+		int[] numbers = new int[10_000];
+		
+		for(int i : a) {
+			numbers[i]++;
+		}
+		
+		int largest = 0;
+		int largestNumber = 0;
+		for (int i = 0; i < numbers.length; i++) {
+			if (largest < numbers[i]) {
+				largest = numbers[i];
+				largestNumber = i;
+			}
+		}
+		
+		
+		return largest == 0 ? -1 : largestNumber;
+	}
+}
