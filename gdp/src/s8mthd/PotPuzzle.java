adac16dc4e36b240afc63dd07b091456d72f0763
flamion <flamion@protonmail.com>
2022-11-28 20:29:22 +0100

    Add start of PotPuzzle GDP task

---
+++ gdp/src/s8mthd/PotPuzzle.java
@@ -0,0 +1,55 @@
+package s8mthd;
+
+import java.awt.Color;
+
+import std.ui.MiniApp;
+
+public class PotPuzzle {
+	public static void main(String[] args) {
+		Pot pot = new Pot(0, 1000, 500);
+		pot.show();
+	}
+
+}
+
+class Pot extends MiniApp {
+	int xPos;
+	int max;
+	int current;
+	
+	public Pot(int x, int max, int cur) {
+		this.xPos = x;
+		this.max = max;
+		this.current = cur;
+	}
+	
+	public void pourInto(Pot other) {
+		int space = other.max - other.current;
+		if (space >= this.current) {
+			other.current += this.current;
+			this.current = 0;
+		} else {
+			other.current += space;
+			this.current -= space;
+		}
+	}
+	
+	public int getCurrent() {
+		return this.current;
+	}
+	
+	public int getMaximum() {
+		return this.getMaximum();
+	}
+	
+	public void show() {
+		Color color = Color.BLACK;
+		if (this.current == 1000) {
+			color = Color.GREEN;
+		} else if () {
+			
+		}
+		
+		drawRectangle("Pot" + xPos, xPos, 150, 25, 25, Color.PINK);
+	}
+}
