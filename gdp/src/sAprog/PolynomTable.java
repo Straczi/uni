1cc78eda374a3a59a68e5bb0dadc8123f6c79734
flamion <contact@flamion.dev>
2022-12-12 17:06:15 +0100

    modify PolynomTable

--- gdp/src/sAprog/PolynomTable.java
+++ gdp/src/sAprog/PolynomTable.java
@@ -5,14 +5,13 @@ import java.util.Objects;
 
 public class PolynomTable {
 	public static void main(String[] args) {
-		System.out.println("a");
-		var p = new Polynom(0.5, 3, 1, 0);
-		System.out.println(p);
-		System.out.println(Arrays.toString(p.innerPolynom));
-		var pi = p.derivative();
-		System.out.println(pi);
-		System.out.println(Arrays.toString(pi.innerPolynom));
-		System.out.println("y");
+		Polynom p = new Polynom(0.5, 3, 1, 0);
+		Table t = new Table(p.derivative(), -10, 10, 41);
+		System.out.println(t.getX(0)); // liefert -10
+		System.out.println(t.getX(1)); // leifert -9.5
+		System.out.println(t.getX(40)); // liefert 10
+		System.out.println(t.getY(0));; // liefert 91
+		System.out.println(t.getY(40)); // liefert 211
 	}
 }
 
@@ -62,31 +61,79 @@ class Polynom {
 			return true;
 		}
 
-		if (otherObject instanceof Polynom) {
-			Polynom other = (Polynom) otherObject;
-			if (this.innerPolynom.length == other.innerPolynom.length) {
-				for (int i = 0; i < this.innerPolynom.length; i++) {
-					if (this.innerPolynom[i] != other.innerPolynom[i]) {
-						return false;
-					}
+		boolean equals = true;
+
+		if (otherObject instanceof Polynom other) {
+			Polynom longer;
+			Polynom shorter;
+
+			if (this.innerPolynom.length < other.innerPolynom.length) {
+				longer = other;
+				shorter = this;
+			} else {
+				longer = this;
+				shorter = other;
+			}
+
+			//Count leading zeroes
+			int leadingZeroes = 0;
+			for (int i = 0; i < longer.innerPolynom.length; i++) {
+				if (longer.innerPolynom[i] != 0.0d) {
+					break;
+				}
+				leadingZeroes++;
+			}
+
+			for (int i = 0; i < longer.innerPolynom.length; i++) {
+				if (
+						((longer.innerPolynom.length - i) <= shorter.innerPolynom.length)
+						&& longer.innerPolynom[longer.innerPolynom.length - 1 - i] != shorter.innerPolynom[shorter.innerPolynom.length - 1 - i]
+				) {
+
 				}
 
-				return true;
-			} else {
-				// TODO
+
+				if (this.innerPolynom[i] != other.innerPolynom[i]) {
+					equals = false;
+					break;
+				}
 			}
 		}
 
-		return false;
+		return equals;
 	}
 }
 
 class Table {
 	double[] values;
+	int from;
+	int to;
+	int steps;
 
-	public Table(Polynom polynom, int from, int to, int stepSize) {
+	public Table(Polynom polynom, int from, int to, int steps) {
+		double[] values = new double[steps];
+		double stepSize = ((double) steps) / (to - from);
 
+		double x = from;
+		for (int i = 0; i < values.length; i++) {
+			double value = 0;
+			for (int j = 0; j < polynom.innerPolynom.length; j++) {
+				value += polynom.innerPolynom[j] * Math.pow(from, polynom.innerPolynom.length - 1 - j);
+			}
+			values[i] = value;
+		}
+
+		this.values = values;
+		this.from = from;
+		this.to = to;
+		this.steps = steps;
 	}
 
+	public int getX(int i) {
+		
+	}
 
+	public double getY(int x) {
+		return this.values[x];
+	}
 }
